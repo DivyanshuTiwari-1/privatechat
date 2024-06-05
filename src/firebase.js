da4841462706ee5bce65c9555c486aa97c1ed1be
DiyanshuTiwaari <sontitiwa569@gamil.com>
2024-06-05 07:39:33 +0530

    removed my credentials from firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBOaMJ5cuWBFrRVoNxQjnGZoAkTXgZ9iVs",
-    authDomain: "chatapp2-3e7ae.firebaseapp.com",
-    projectId: "chatapp2-3e7ae",
-    storageBucket: "chatapp2-3e7ae.appspot.com",
-    messagingSenderId: "522941962162",
-    appId: "1:522941962162:web:8dfd9032c3d1c0f959f6ab",
-    measurementId: "G-S1SWKJNKTS"
+    //add your firebaseconfig here
 };
 
 // Initialize Firebase
